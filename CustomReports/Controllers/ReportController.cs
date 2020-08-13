c8a2bed734e0ca21a248ceb0dfdc7f873cd2044f
wissem khrarib <khraribwissem@gmail.com>
2020-08-13 14:48:56 +0200

    initial commit

---
+++ CustomReports/Controllers/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using CustomReports.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomReports.Controllers
+{
+    public class ReportController : Controller
+    {
+        private ReportsDbContext dbContext = new ReportsDbContext();
+     
+        // GET: Report
+        public ActionResult Index()
+        {
+            var reports = dbContext.Reports.ToList();
+            return View(reports);
+        }
+
+        [HttpPost]
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var report = dbContext.Reports.FirstOrDefault(r => r.Id == id);
+            return View(report);
+        }
+    }
+}
